a57d2401602e4b562173caeb3be52532072c92b6
The-Lake <levkleshchenko@gmail.com>
2023-04-29 21:37:21 +0200

    added hover side menu

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-document.addEventListener('DOMContentLoaded', (e) => {
-    document.getElementById('open-info').addEventListener('click', (e => {
-        document.getElementById('_info').classList.add('show_div')
-    })) 
+document.addEventListener("DOMContentLoaded", (e) => {
+  document.getElementById("open-info").addEventListener("click", (e) => {
+    document.getElementById("_info").classList.add("show_div");
+  });
 
-    document.getElementById('close').addEventListener('click', (e => {
-        document.getElementById('_info').classList.remove('show_div')
-    })) 
-})
-
+  document.getElementById("close").addEventListener("click", (e) => {
+    document.getElementById("_info").classList.remove("show_div");
+  });
+});
 
 /* Убрать перезагрузку при нажатии (форма)
 document.getElementById('open-info').addEventListener('submit', (e => {
     e.preventDefault()
 }))
 */
-
- 
