5c5d2a51423ae69b8bc3004e4fa6cacef1e3ea2d
The-Lake <levkleshchenko@gmail.com>
2023-04-29 18:46:51 +0200

    main.js open ifo

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', (e) => {
-    
+    document.getElementById('open-info').addEventListener('click', (e => {
+        document.getElementById('_info').classList.add('show_div')
+    })) 
+
+    document.getElementById('close').addEventListener('click', (e => {
+        document.getElementById('_info').classList.remove('show_div')
+    })) 
 })
+
+
+/* Убрать перезагрузку при нажатии (форма)
+document.getElementById('open-info').addEventListener('submit', (e => {
+    e.preventDefault()
+}))
+*/
+
+ 
